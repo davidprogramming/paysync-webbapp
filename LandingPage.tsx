7548741eeb4734f1d0de90b5c93a5e5a04455aa7
davidprogramming <67407092+davidprogramming@users.noreply.github.com>
2025-02-13 11:39:35 +0200

    Update LandingPage.tsx

--- LandingPage.tsx
+++ LandingPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { LoginForm, SignupForm } from '../components/AuthForms';
+import { LoginForm, SignupForm } from './components/AuthForms';
 import {
   Briefcase,
   Globe,
